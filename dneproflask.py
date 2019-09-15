bb6b916004d1940b86c8d453b194e803158a7653
kosdp <kosdp@users.noreply.github.com>
2019-09-15 21:20:41 +0300

    mjpeg streaming added

--- dneproflask.py
+++ dneproflask.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, Response, render_template, stream_with_context
 from base64 import b64encode
 import requests
 
@@ -7,6 +7,7 @@ app = Flask(__name__)
 
 # cams
 @app.route('/')
+@app.route('/cams')
 def get_cams():
     cams = []
     for num in range(60):
@@ -15,16 +16,18 @@ def get_cams():
         resp = requests.get(url, headers=headers)
         if resp.status_code == requests.codes.ok and len(resp.content) > 0:
             img = 'data:image/jpg;base64,' + b64encode(resp.content).decode()
-            #params = 'width="1280" height="720"'
-            params = 'width="640" height="360"'
-            cam = '<a href="http://dnepro.net/cams/' + str(num).zfill(2) + '"><br><img ' + params + ' src="' + img + '"></a><br>'
+            params = 'width="960" height="540"'
+            cam = '<a href="/cam/' + str(num) + '"><br><img ' + params + ' src="' + img + '"></a><br>'
             cams.append({'data': cam, 'name': num })
-    return render_template('cams.html', cams=cams)
+    return render_template('cams.html', cams = cams)
 
 # cam
 @app.route('/cam/<cam>')
 def show_cam(cam):
-    return 'here is cam ' + cam
+    url = 'http://camera.dnepro.net/camera' + cam + '.jpg'
+    headers = {'Referer':'http://dnepro.net/cams/'}
+    resp = requests.get(url, headers = headers, stream = True)
+    return Response(response = stream_with_context(resp.iter_content(chunk_size = 8192)), content_type = resp.headers['Content-Type'], status = resp.status_code)
 
 # plates
 @app.route('/plates')
@@ -34,4 +37,4 @@ def get_plates():
         url = 'http://connect.dnepro.net/cams/' + str(num).zfill(2) + '.txt'
         plate = requests.get(url).text + '<br>'
         plates.append({'data': plate, 'name': num })
-    return render_template('cams.html', cams=plates)
+    return render_template('cams.html', cams = plates)
