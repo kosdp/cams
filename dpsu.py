c00fda809c31674e8959e1a66d0c543fb1cb0d9c
kosdp <kosdp@users.noreply.github.com>
2019-09-23 19:28:39 +0300

    dpsu

---
+++ dpsu.py
@@ -0,0 +1,3 @@
+# curl -v -A "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0" https://dpsu.gov.ua/ua/border/
+# curl -v https://stream.dpsu.gov.ua:5101/005_Krakivec_rear/index.m3u8
+# curl -v https://stream.dpsu.gov.ua:5101/005_Krakivec_rear/embed.html
