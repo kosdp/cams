985420f6e2a9c664c1b6c87ea386aad636f945c8
kosdp <kosdp@users.noreply.github.com>
2019-09-25 19:21:24 +0300

    Dnepr cameras

---
+++ dnepr.py
@@ -0,0 +1,19 @@
+# https://cam.dnepr.com
+# https://cam.dnepr.com/hls/num1.m3u8
+
+import requests
+import urllib3
+urllib3.disable_warnings()
+from bs4 import BeautifulSoup
+import re
+
+chrome_ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
+url_cam = 'https://cam.dnepr.com'
+headers = {'User-Agent': chrome_ua}
+resp = requests.get(url_cam, headers = headers, verify = False)
+
+soup = BeautifulSoup(resp.text, 'lxml')
+cams = { cam['href']:cam.text for cam in soup.find_all('a', {'href':re.compile('^index\.php\?cam=.*$')}) }
+for cam in cams:
+    if not 'нет' in cams[cam]:
+        print(cams[cam], '|', re.sub('$', '.m3u8', re.sub('index\.php\?cam=', 'ffplay https://cam.dnepr.com/hls/num', cam)))
